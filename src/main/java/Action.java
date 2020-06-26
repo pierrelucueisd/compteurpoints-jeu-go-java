be1b01140d892454941ddc32d897b8c88e6658bb
Pierre-Luc Maître <pierrelucueisd@gmail.com>
2020-06-26 14:30:16 -0400

    retrait des persistance erreurs dans action

--- src/main/java/Action.java
+++ src/main/java/Action.java
@@ -2,12 +2,10 @@ import java.util.Optional;
 
 public class Action {
     private final ActionType type;
-    private ErrorType error;
     private final Optional<Position> position;
 
     public Action(ActionType type, Optional<Position> position) {
         this.position = position;
-        this.error = ErrorType.None;
         this.type = type;
     }
 
@@ -17,10 +15,6 @@ public class Action {
 
     public Optional<Position> getPosition() {
         return position;
-    }
-
-    public ErrorType getError() {
-        return error;
     }
 
     public void execute(Board b, Player p) {
