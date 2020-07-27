5a448cf09cb311470c27a58ddf15e830e29d691a
Pierre-Luc Maître <pierrelucueisd@gmail.com>
2020-07-26 21:05:13 -0400

    amélioration du board constructor et début d'une fonction de getAdjacences transitives avec prédicat

--- src/main/java/Board/Builder/BoardBuilderFromBoardRepresentation.java
+++ src/main/java/Board/Builder/BoardBuilderFromBoardRepresentation.java
@@ -1,11 +1,13 @@
 package Board.Builder;
 
 import Board.Board;
+import Board.Intersection;
 import Board.Position;
 import Player.Color;
 
 import java.util.Optional;
 
+//construit un board sans enlever les zones capturées
 public class BoardBuilderFromBoardRepresentation implements BoardBuilder {
     String str;
     public BoardBuilderFromBoardRepresentation(String _str) {
@@ -24,12 +26,17 @@ public class BoardBuilderFromBoardRepresentation implements BoardBuilder {
             if(occupations.length != b.getSize()) return Optional.empty();
             for(int j = 0; j<occupations.length; j++) {
                 String occupation = occupations[j];
-                if(occupation.equals("○")) b.putStone(Color.Black, new Position(j, noLigneMat));
-                else if(occupation.equals("●")) b.putStone(Color.White, new Position(j, noLigneMat));
+                if(occupation.equals("○")) putStone(b, new Position(j, noLigneMat), Color.Black);
+                else if(occupation.equals("●")) putStone(b, new Position(j, noLigneMat), Color.White);
                 else if(!occupation.equals("+")) return Optional.empty();
             }
             noLigneMat++;
         }
         return Optional.of(b);
+    }
+
+    private void putStone(Board b, Position p, Color c) {
+        Intersection intersection = b.getIntersection(p);
+        intersection.setOccupation(c);
     }
 }
