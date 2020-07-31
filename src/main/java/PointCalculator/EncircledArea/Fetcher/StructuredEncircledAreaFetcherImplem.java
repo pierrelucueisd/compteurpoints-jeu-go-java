7fbf31d37d3179e72b4d99443383bc37e1cab897
Pierre-Luc Maître <pierrelucueisd@gmail.com>
2020-07-31 14:20:42 -0400

    ajout de l'inversion de conytôle pour StructuredEncircledAreaFetcher

---
+++ src/main/java/PointCalculator/EncircledArea/Fetcher/StructuredEncircledAreaFetcherImplem.java
@@ -0,0 +1,26 @@
+package PointCalculator.EncircledArea.Fetcher;
+
+import Board.Board;
+import PointCalculator.EncircledArea.EncircledArea;
+import PointCalculator.EncircledArea.Validator.EncircledAreaValidator;
+
+import java.util.List;
+
+public class StructuredEncircledAreaFetcher {
+
+    private Board b;
+    private EncircledAreaValidator rootValidator;
+
+    public StructuredEncircledAreaFetcher(Board b, EncircledAreaValidator rootValidator) {
+        this.b = b;
+        this.rootValidator = rootValidator;
+    }
+
+    //retourne les racines
+    public List<EncircledArea> fetch() {
+        EncircledAreaFlatListFetcher flarListFecther = new EncircledAreaFlatListFetcher(b);
+        List<EncircledArea> areas = flarListFecther.fetchFlatListFromBoard();
+        EncircledAreaStructurator structurator = new EncircledAreaStructurator(b, rootValidator);
+        return structurator.structurateElementsOfList(areas);
+    }
+}
